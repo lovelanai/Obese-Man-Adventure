0e894774b9da7924a5b4c5178728d74547c7f03a
Love Lanai <love.lanai@medieinstitutet.se>
2021-11-14 14:24:34 +0100

    started configuring game

--- main.js
+++ main.js
@@ -1,13 +1,77 @@
-var audio = document.getElementById('audio');
-var playPauseBTN = document.getElementById('playPauseBTN');
-var count = 0;
+var bleep = new Audio();
+bleep.src = "assets/music/Mario-coin-sound.mp3";
 
-function playPause(){
-    if(count == 0){
-        count = 1;
-        audio.pause();
-    } else{
-        count = 0;
-        audio.play();
+
+var song = new Audio();
+song.src = "/assets/music/Jeremy Blake - Powerup!.mp3";
+
+
+var slurp = new Audio();
+slurp.src = "/assets/music/Mario Warp Pipe Sound Effect.mp3";
+
+
+function myFunction() {
+    var myobj = document.getElementById("startbtn");
+    myobj.remove();
+    document.getElementById('game').style.display = "flex";
+  }
+
+
+
+  var elem = document.getElementById("game");
+  function openMusic() {
+
+    var myobj = document.getElementById("musicbtn");
+    myobj.remove();
+
+    document.getElementById('gamemenu').style.display = "flex";
+
+  }
+
+
+
+
+
+
+   function entername() {
+    var person = prompt("Please enter your name", "");
+    if (person != null) {
+      document.getElementById("demo").innerHTML =
+      person + "! Last chance to back out..";
+      
     }
-}
+    else {
+      alert("Are you stupid? The game hasn't even started yet... try again");
+  }
+  var myobj = document.getElementById("entername");
+  myobj.remove();
+
+  document.getElementById('entergame').style.display = "flex";
+
+
+
+    }
+
+
+    $(document).ready(function() {
+      var target = $("#game");
+      $("#entergame").click(function() {
+        removeElement(target);
+      });
+    });
+    
+    function removeElement(target) {
+      target.animate({
+        opacity: "-=1"
+      }, 1000, function() {
+        target.remove();
+      });
+    }
+function entergametext(){
+    document.getElementById('start-text').style.display = "flex";}
+  
+
+    function stop() {
+      song.pause();
+      song.currentTime = 0;
+    }
