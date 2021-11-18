d19820b80db3df2a4f2958cdaf7c67ccc953f424
Love Lanai <love.lanai@medieinstitutet.se>
2021-11-18 01:48:41 +0100

    add content to game

--- main.js
+++ main.js
@@ -1,32 +1,26 @@
-function firstfunction(){
-var myobj = document.getElementById("firstfunction");
-myobj.remove();
-document.getElementById('gamepage').style.display = "block";
+function firstfunction() {
+  var myobj = document.getElementById("firstfunction");
+  myobj.remove();
+  document.getElementById('gamepage').style.display = "block";
 }
 
 
 function myFunction() {
-    var myobj = document.getElementById("startbtn");
-    myobj.remove();
-    document.getElementById('game').style.display = "flex";
-  }
+  var myobj = document.getElementById("startbtn");
+  myobj.remove();
+  document.getElementById('game').style.display = "flex";
+}
 
 
 
-  var elem = document.getElementById("game");
-  function openMusic() {
+var elem = document.getElementById("game");
+function entermainmenu() {
 
-    var myobj = document.getElementById("musicbtn");
-    myobj.remove();
+  var myobj = document.getElementById("fullscreenbtn");
+  myobj.remove();
 
-    document.getElementById('gamemenu').style.display = "flex";
-
-  }
-
-
-
-
-
+  document.getElementById('gamemenu').style.display = "flex";
+}
 
 function begin() {
 
@@ -34,158 +28,320 @@ function begin() {
   myobj.remove();
 
   document.getElementById('nameinput').style.display = "flex";
-
-
-
-    }
-
-    function entername() {
-      var person = document.getElementById("userinput").value;
-      if (person != null) {
-        document.getElementById("demo").innerHTML =
-        person + "! Last chance to back out..";
-        
-      }
-
-      var myobj = document.getElementById("nameinput");
-      myobj.remove();
-    
-      document.getElementById('entergame').style.display = "flex";
 }
-    
-    
-    
+
+function entername() {
+  var person = document.getElementById("userinput").value;
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+      person + "! Last chance to back out..";
+
+  }
+
+  var myobj = document.getElementById("nameinput");
+  myobj.remove();
+
+  document.getElementById('entergame').style.display = "flex";
+}
+
+
+$(document).ready(function () {
+  var target = $("#game");
+  $("#entergame").click(function () {
+    removeElement(target);
+  });
+
+});
+
+function removeElement(target) {
+  target.animate({
+    opacity: "-=1"
+  }, 1000, function () {
+    target.remove();
+  });
+}
+
+function enterscene1() {
+  document.getElementById('scene1').style.display = "initial";
+}
+
+
+var elem = document.getElementById("gamefullscreen");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  }
+}
+
+
+function stop() {
+  song.pause();
+  song.currentTime = 0;
+}
 
 
 
 
-    $(document).ready(function() {
-      var target = $("#game");
-      $("#entergame").click(function() {
-        removeElement(target);
-      });
-      
-    });
-    
-    function removeElement(target) {
-      target.animate({
-        opacity: "-=1"
-      }, 1000, function() {
-        target.remove();
-      });
+
+
+
+
+// SCENE 1
+
+
+function changetext(text) {
+  const paragraph = document.getElementById("text")
+  paragraph.innerText = text;
+}
+
+
+$(document).ready(function () {
+  var target = $("#lore");
+  $("#continue").click(function () {
+    removeElement(target);
+  });
+});
+
+function removeElement(target) {
+  target.animate({
+    opacity: "-=1"
+  }, 1000, function () {
+    target.remove();
+  });
+
+}
+
+function house() {
+  var myobj = document.getElementById("continue");
+
+  document.getElementById('house').style.display = "flex";
+
+  changetext("This is it. Finally we can face the evil MC.DAVID! He will soon regret stealing my lunch 10 years ago.")
+}
+
+
+function nextpage() {
+
+  var myobj = document.getElementById("textone");
+  myobj.remove();
+
+  document.getElementById('texttwo').style.display = "flex";
+
+  changetext("Let's enter the evil mansion and kick him in the sack!")
+
+
+}
+
+function closepage() {
+
+  var myobj = document.getElementById("texttwo");
+  myobj.remove();
+
+  document.getElementById('arrow').style.display = "flex";
+
+}
+
+
+$(document).ready(function () {
+  var target = $("#scene1");
+  $("#arrow").click(function () {
+    removeElement(target);
+  });
+});
+
+function removeElement(target) {
+  target.animate({
+    opacity: "-=1"
+  }, 1000, function () {
+    target.remove();
+  });
+}
+
+function gotoscene2() {
+  document.getElementById('scenetwo').style.display = "flex";
+}
+
+
+// SCENE 2
+
+function changetext2(text) {
+  const paragraph = document.getElementById("text2")
+  paragraph.innerText = text;
+}
+
+$(document).ready(function () {
+  var target = $("#lore2");
+  $("#enterhouse").click(function () {
+    removeElement(target);
+  });
+});
+
+function removeElement(target) {
+  target.animate({
+    opacity: "-=1"
+  }, 1000, function () {
+    target.remove();
+  });
+
+}
+
+
+function enterhouse() {
+
+
+  document.getElementById('scene2fullscreen').style.display = "flex";
+
+  changetext2("The evil MC.DAVID is upstairs... He probably still have some of my baloney sandwitch between his teeth..")
+}
+
+
+function s2nextpage() {
+
+  var myobj = document.getElementById("s2textone");
+  myobj.remove();
+
+  document.getElementById('s2texttwo').style.display = "flex";
+
+  changetext2("For now, I'm way to fat to take the stairs.. Let's find another way up!...")
+
+}
+
+
+function s2closepage() {
+
+  var myobj = document.getElementById("s2texttwo");
+  myobj.remove();
+
+  document.getElementById('s2arrow1').style.display = "flex";
+  document.getElementById('s2arrow2').style.display = "flex";
+  document.getElementById('s2arrow3').style.display = "flex";
+
+}
+
+function openNav() {
+  document.getElementById("myNav").style.width = "17rem";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+function showelevatorpic() {
+
+  document.getElementById('elevatorpic').style.display = "flex";
+
+
+  changetext2("Wha..??? WHAT IS THIS????!! ")
+
+
+}
+
+function changepicture() {
+
+
+  document.getElementById('brokenframe').style.display = "flex";
+  document.getElementById('changepicture2').style.display = "initial";
+
+  document.getElementById('changepicturebtn').style.display = "none";
+  document.getElementById('changepicture').style.display = "none";
+
+
+
+  changetext2("BASTARD!!!... He must have known I was coming!... If i weren't so fat, I would run upstairs and kick his sack!...")
+
+
+}
+
+function changepicture2() {
+
+
+  document.getElementById('brokenframe').style.display = "none";
+  document.getElementById('elevatorpic').style.display = "none";
+  document.getElementById('s2arrow2').style.display = "none";
+  document.getElementById('noelevator').style.display = "flex";
+
+}
+
+function noelevator(){
+  document.getElementById('fatcry').style.display = "initial";
+  document.getElementById('elevatorpic').style.display = "flex";
+  changetext2("Well as you already know, I'm too fat for the elevator.. Let's find another way up!!..")
+}
+
+
+function gotoscene3(){
+  document.getElementById('scenetwo').style.display = "none";
+  document.getElementById('scenethree').style.display = "flex";
+  changetext3("Wow.. A room full of f*ckn junk... Maybe I can find something useful in the back!..")
+}
+
+// SCENE 3, SCIENCEROOM FIRST VISIT
+
+function changetext3(text) {
+  const paragraph = document.getElementById("text3")
+  paragraph.innerText = text;
+}
+
+
+
+
+
+function s3text1(){
+  var myobj = document.getElementById("s3text1");
+  myobj.remove();
+
   
-    }
+  document.getElementById('s3arrow').style.display = "flex";
 
-    function enterscene1(){
-      document.getElementById('scene1').style.display = "initial";
-    }
+}
 
 
-    var elem = document.getElementById("gamefullscreen");
-    function openFullscreen() {
-      if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-      } else if (elem.webkitRequestFullscreen) {
-        elem.webkitRequestFullscreen();
-      } else if (elem.msRequestFullscreen) {
-        elem.msRequestFullscreen();
-      }
-    }
+function bubblebox(){
+document.getElementById('bubblebox').style.display = "flex";
+document.getElementById('s3arrow').style.display = "none";
+changetext3("HEY!!!! Unless you got something of value..., GET OUT FAT MAN!!!")
+
+}
+
+function s3text2(){
+  document.getElementById('s3text2').style.display = "flex";
+  document.getElementById('sadbigman').style.display = "initial";
+  document.getElementById('bigman').style.display = "none";
 
 
-    function stop() {
-      song.pause();
-      song.currentTime = 0;
-    }
+  var myobj = document.getElementById("bubblebox");
+  myobj.remove();
+
+  changetext3("WTF... Maybe he won't be an asshole and help me if I bring him something of value!...")
+  }
 
 
+function closes3text2(){
+  document.getElementById('s3arrow2').style.display = "flex";
 
+  var myobj = document.getElementById("s3text2");
+  myobj.remove();
+}
 
+function leavescienceroom1(){
+  document.getElementById('scenetwo').style.display = "flex";
+  document.getElementById('s2textafterscene3').style.display = "flex";
+  document.getElementById('scenethree').style.display = "none";
+  changetextafterscene3("Hmmmm... Valuable items for that guy...? Maybe he's a big fat man like me??? Where can I find items for a Fat dude?")
 
+}
 
+function changetextafterscene3(text) {
+  const paragraph = document.getElementById("textafterscene3")
+  paragraph.innerText = text;
+}
 
-
-    // GAME 2 
-
-
-    function changetext(text) { 
-      const paragraph = document.getElementById("text")
-      paragraph.innerText = text;
-      }
-      
-      
-      
-      
-      $(document).ready(function () {
-          var target = $("#lore");
-          $("#continue").click(function () {
-              removeElement(target);
-          });
-      });
-      
-      function removeElement(target) {
-          target.animate({
-              opacity: "-=1"
-          }, 1000, function () {
-              target.remove();
-          });
-      
-      }
-      
-      function house() {
-          var myobj = document.getElementById("continue");
-      
-      
-          document.getElementById('house').style.display = "flex";
-      
-          changetext("This is it. Finally we can face the evil MC.DAVID! He will soon regret stealing my lunch 10 years ago.")
-      
-      
-      
-      }
-
-      
-      function nextpage() {
-      
-        var myobj = document.getElementById("textone");
-        myobj.remove();
-      
-        document.getElementById('texttwo').style.display = "flex";
-      
-        changetext("Let's enter the evil mansion and kick him in the sack!")
-       
-      
-      }
-      
-      
-      function closepage() {
-      
-        var myobj = document.getElementById("texttwo");
-        myobj.remove();
-      
-        document.getElementById('arrow').style.display = "flex";
-      
-      }
-      
-      
-      $(document).ready(function() {
-          var target = $("#scene1");
-          $("#arrow").click(function() {
-            removeElement(target);
-          });
-        });
-        
-        function removeElement(target) {
-          target.animate({
-            opacity: "-=1"
-          }, 1000, function() {
-            target.remove();
-          });
-        }
-      
-      function gotoscene2(){
-        document.getElementById('scenetwo').style.display = "flex";
-      }
-      
-
-      
+function afterscienceclose(){
+  var myobj = document.getElementById("s2textafterscene3");
+  myobj.remove();
+}
