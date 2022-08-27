0584dd5f22c9630849bf05e63239086e38014ae5
Love Lanai <love.lanai@medieinstitutet.se>
2022-08-27 16:50:47 +0200

    obese-man-adventure

---
+++ README.md
@@ -0,0 +1,25 @@
+# OBESE MAN ANDVENDURE IV
+
+## Background
+This is a demo for a full version web-game (not yet created) called OBESE MAN ADVENTURE IV! The game is created by using html, css and javascript.
+
+## Installation
+
+1. Unzip the file
+2. Open index.html
+3. Enjoy
+
+
+
+## Contributing
+Game written and created by Love Lanai
+***
+Pull requests are always fun. For major changes, please open an issue first to discuss what you would like to change.
+
+
+## Demo link
+[OBESE-MAN-ADVENTURE DEMO](https://lovelanai.github.io/Obese-Man-Adventure/)
+
+## License
+
+Some, if not all audio are NOT owned or created by me. So do NOT use this game for commercial  purposes in any instance. 
---
+++ js/audio.js
@@ -0,0 +1,81 @@
+var bleep = new Audio();
+bleep.src = "../assets/music/Mario-coin-sound.mp3";
+
+var song = new Audio();
+song.src = "../assets/music/Jeremy Blake - Powerup!.mp3";
+
+var slurp = new Audio();
+slurp.src = "../assets/music/Mario Warp Pipe Sound Effect.mp3";
+
+var downsound = new Audio();
+downsound.src = "../assets/music/blur.mp3";
+
+var among = new Audio();
+among.src = "../assets/music/amongus.mp3";
+
+var hit = new Audio();
+hit.src = "../assets/music/8 bit impact sound effect (volume up).mp3";
+
+var magicstart = new Audio();
+magicstart.src = "../assets/music/Magic Game Start Sound Effect.mp3";
+
+var warp = new Audio();
+warp.src = "../assets/music/Mariowarp.mp3";
+      
+var spooky = new Audio();
+spooky.src = "../assets/music/Kevin MacLeod - 8bit Dungeon Level.mp3";
+
+var outside = new Audio();
+outside.src = "../assets/music/outside.mp3";
+
+var clicksound = new Audio();
+clicksound.src = "../assets/music/clicksound.mp3";
+
+var opendoor = new Audio();
+opendoor.src = "../assets/music/opendoor.mp3";
+
+var bowser = new Audio();
+bowser.src = "../assets/music/bowser.mp3";
+
+var inventory = new Audio();
+inventory.src = "../assets/music/inventory.mp3";
+
+var what = new Audio();
+what.src = "../assets/music/what.mp3";
+
+var kick = new Audio();
+kick.src = "../assets/music/kick.mp3";
+
+var error = new Audio();
+error.src = "../assets/music/error.mp3";
+
+var labroom = new Audio();
+labroom.src = "../assets/music/labroom.mp3";
+
+var scream = new Audio();
+scream.src = "../assets/music/angrywoman.mp3";
+
+var mmm = new Audio();
+mmm.src = "../assets/music/happywoman.mp3";
+
+var chest = new Audio();
+chest.src = "../assets/music/chest.mp3";
+
+var unlock = new Audio();
+unlock.src = "../assets/music/unlock.mp3";
+
+
+var squeek = new Audio();
+squeek.src = "../assets/music/squeek.mp3";
+
+var final = new Audio();
+final.src = "../assets/music/finalscene.mp3";
+
+var kitchen = new Audio();
+kitchen.src = "../assets/music/kitchen.mp3";
+
+var fredrik = new Audio();
+fredrik.src = "../assets/music/fredrik.mp3";
+
+var omg = new Audio();
+omg.src = "../assets/music/omg.mp3";
